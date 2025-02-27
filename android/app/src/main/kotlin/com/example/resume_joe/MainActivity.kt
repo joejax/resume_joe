b215c1ef17948e999b2da3f96bfd0221af9847a5
Kusumeshkant Sharma <kusumeshkantsharma@Kusumeshkants-MacBook-Air.local>
2025-02-27 10:34:00 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/resume_joe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.resume_joe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,409 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(left: 10, top: 10, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: height * .17,
+                    width: width * .4,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * .16,
+                    width: width * .55,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Kant',
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.work),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Text(
+                              'Job Profile',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.location_city),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Text(
+                              'Location',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: height * .3,
+                width: width,
+                child: Text(
+                  'Description',
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+              Text(
+                'Companies Work for:-',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Soft skills:-',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: height * .17,
+                      width: width * .4,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+              // Container(
+              //   height: height * .2,
+              //   width: width * 2,
+              //   color: Colors.amber,
+              //   child: Row(
+              //     children: [],
+              //   ),
+              // ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Educatuion:-',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              /* Contact Detail */
+
+              Text(
+                'Contact detail:-',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    'Kusumeshkant2013@mailinator.com',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    '8754886595',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Bootstrap.whatsapp,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    '8754886595',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 6,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              /* Social Detail */
+              Text(
+                'Social Networks:-',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      child: Icon(
+                        Bootstrap.facebook,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                      child: Icon(Bootstrap.google),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                      child: Icon(Bootstrap.github),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                      child: Icon(Bootstrap.linkedin),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
